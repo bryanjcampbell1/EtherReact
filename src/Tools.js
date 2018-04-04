ff774d2ea947a5a9325c1b52bec3ca57bfe5ea11
Bryan Campbell <bryanjcampbell1@gmail.com>
2018-04-03 22:31:59 -0400

    got function call working i think. it brings up metamask at least

--- src/Tools.js
+++ src/Tools.js
@@ -8,6 +8,64 @@ const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
 
 var $ = require('jquery');
 
+var abiData = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getName",
+    "outputs": [
+      {
+        "name": "winnerName_",
+        "type": "bytes32"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "buyIn",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "yourName",
+        "type": "bytes32"
+      }
+    ],
+    "name": "setName",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  }
+];
+
+
+var gasVal = 25000;
+var gasPrice = 40000000000;
+
+var gasHex = '0x' + gasVal.toString(16);
+var gpHex = '0x' + gasPrice.toString(16);
+
+var contractAddress = "0x65601fcf248988bb79e1358d77edcc649f7a548a";
+
+var simpleContract = new web3.eth.Contract(abiData);
+simpleContract.options.address = contractAddress;
+
 var hexToBytes = function(hex) {
   for (var bytes = [], c = 0; c < hex.length; c+=2)
   bytes.push(parseInt(hex.substr(c, 2), 16));
@@ -39,7 +97,21 @@ class Tools extends Component {
                     });
     }
     handleClick(e) {
-      alert(this.textInput.value);
+      var textValue = this.textInput.value;
+      var textBytes = web3.utils.asciiToHex(textValue);
+
+      web3.eth.getAccounts(function(error, result) {
+          if(error != null)
+              console.log("Couldnt get accounts");
+
+         simpleContract.methods.setName(textBytes).send({from: result[0]}, function(error, result){
+                                 if(!error)
+                                     console.log(JSON.stringify(result));
+                                 else
+                                     console.error(error);
+                             });
+
+      });
 
     }
 
